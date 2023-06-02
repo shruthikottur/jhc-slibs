83d65c28f500444cc1706f89f2c71b73d612b196
shruthikottur <132871715+shruthikottur@users.noreply.github.com>
2023-06-02 15:35:10 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,5 +1,5 @@
 def call(user,ip,credId,warName){
-  sshagent(['credId']) {
+  sshagent([credId]) {
     sh "scp -o StrictHostKeyChecking=no target/${warName}.war ${user}@${ip}:/opt/tomcat9/webapps/"
     sh "ssh ${user}@${ip} /opt/tomcat9/bin/shutdown.sh"
       sh "ssh ${user}@${ip} /opt/tomcat9/bin/startup.sh"
